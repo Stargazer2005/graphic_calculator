c48a9ea5d70f8b8ae94d6123cb77e2ac9378c472
Ilya_Rybalkin <142895755+Stargazer2005@users.noreply.github.com>
2023-11-15 01:13:51 +0300

    First version of graphic window class

---
+++ graphic_window.h
@@ -0,0 +1,19 @@
+#ifndef GRAPH_WIN_H
+#define GRAPH_WIN_H
+
+#include <Graph_Lib/Graph.h>
+#include <Graph_lib/GUI.h>
+#include <Graph_lib/SImple_window.h>
+#include <string>
+using namespace Graph_lib;
+
+class Graphic_window : public Simple_window
+{
+public:
+  Graphic_window(Point xy, int w, int h, const std::string& title, int scale);
+
+private:
+  int s;
+  Axis x_, y_;
+};
+#endif  // GRAPH_WIN_H
