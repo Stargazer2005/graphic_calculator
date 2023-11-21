646b908c1ace92c554d35b96cee712be64f4881d
Ilya_Rybalkin <rybalkin.id@phystech.edu>
2023-11-22 01:28:02 +0300

    Menu first version

--- graphic_window.h
+++ graphic_window.h
@@ -1,19 +1,69 @@
 #ifndef GRAPH_WIN_H
 #define GRAPH_WIN_H
 
-#include <Graph_Lib/Graph.h>
 #include <Graph_lib/GUI.h>
-#include <Graph_lib/SImple_window.h>
+#include <Graph_lib/Graph.h>
+#include <Graph_lib/Window.h>
 #include <string>
-using namespace Graph_lib;
 
-class Graphic_window : public Simple_window
+using Graph_lib::Address;
+using Graph_lib::Axis;
+using Graph_lib::Button;
+using Graph_lib::In_box;
+using Graph_lib::Point;
+
+constexpr double scale_coef = 1.2;
+
+class Graphic_window : public Graph_lib::Window
 {
 public:
   Graphic_window(Point xy, int w, int h, const std::string& title, int scale);
 
+  void wait_for_button ()
+  {
+    while (!button_pushed && Fl::wait())
+      ;
+    button_pushed = false;
+    Fl::redraw();
+  }
+
+  bool window_is_open () { return !quit_button_pushed; }
+
+  std::string get_func () { return func_string; }
+
 private:
   int s;
   Axis x_, y_;
+  std::string func_string = "";
+
+  Button quit_button;
+  Button incr_button;
+  Button decr_button;
+  Button draw_button;
+  In_box enter_box;
+
+  bool button_pushed{false};
+  bool quit_button_pushed{false};
+
+  static void cb_quit (Address, Address widget);
+  static void cb_incr (Address, Address widget);
+  static void cb_decr (Address, Address widget);
+  static void cb_draw (Address, Address widget);
+
+  void quit ()
+  {
+    hide();
+    quit_button_pushed = true;
+  }
+
+  void increase_scale () { s = int(s * scale_coef); }
+
+  void decrease_scale () { s = int(s / scale_coef); }
+
+  void draw_graph ()
+  {
+    func_string = enter_box.get_string();
+    button_pushed = true;
+  }
 };
 #endif  // GRAPH_WIN_H
--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include "graphic_window.h"
 #include <Graph_Lib/Graph.h>
 #include <Graph_lib/GUI.h>
-#include <Graph_lib/Simple_window.h>
+#include <Graph_lib/Window.h>
+#include <iostream>
 
 using namespace Graph_lib;
 
 int main ()
 {
   Graphic_window grw(Point(100, 100), 1000, 700, "DESMOS", 50);
+  std::cout << grw.get_func() << '\n';
+  while (grw.window_is_open())
+  {
+    grw.wait_for_button();
+    std::cout << grw.get_func() << '\n';
+  }
   grw.wait_for_button();
 }
