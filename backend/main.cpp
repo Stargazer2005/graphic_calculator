e5ce778da2eadd68a909e6b944f31728b70ce5cf
Umbrella_Leaf <krivoruchko.di@phystech.edu>
2023-11-22 19:52:14 +0300

    Create function "calc" (using class Stack)

---
+++ backend/main.cpp
@@ -0,0 +1,16 @@
+#include "back_func.h"
+#include "std_lib_facilities.h"
+
+int main ()
+{
+    // string s;
+    // getline(cin, s);
+    // vector<char> Rev_pol = reverse_polish(lexeme(s));
+    vector<string> exemp1{"1.2", "8", "+", "5", "-"};
+    vector<string> exemp2{"x", "4", "2", "*", "1", "5", "-", "2", "^", "/", "+"};
+
+    cout << endl;
+    cout << calc(exemp1, 0) << endl;
+    cout << calc(exemp2, 3);
+    return 0;
+}
