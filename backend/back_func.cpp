d7f807b692f8ba33cd51c6011143ec3cbef6171b
Umbrella_Leaf <krivoruchko.di@phystech.edu>
2023-11-23 17:35:26 +0300

    Create function "checker"

--- backend/back_func.cpp
+++ backend/back_func.cpp
@@ -1,4 +1,5 @@
 #include "back_func.h"
+#include <algorithm>
 #include <stdexcept>
 
 // локальный класс (для взаимодействия с обратной польской записью)
@@ -31,9 +32,7 @@ class Stack
     void print ()
     {
         for (auto i : st)
-        {
             cout << i << ", ";
-        }
         cout << endl;
     }
 
@@ -41,7 +40,49 @@ class Stack
     vector<double> st;
 };
 
-bool checker (const string& expr) {}
+// вспомогательная функция "содержится ли символ в строке?"
+bool c_in_s (char c, const string& s) { return (s.find(c) != string::npos); }
+
+bool checker (const string& expr)
+{
+    const string calc_chars = "x1234567890+-*/^sctelu()";  // строка со всеми разрешенными символами
+    const string oper = "+-*/^";  // строка с операциями
+
+    // проверка на скобочки
+    if (count(expr.begin(), expr.end(), '(') != count(expr.begin(), expr.end(), ')'))
+    {
+        // throw invalid_argument("checker: number of () mismatch");
+        return false;
+    }
+    // первый и последний символы не должны быть знаками
+    if (c_in_s(expr[0], oper) || c_in_s(expr[expr.size() - 1], oper))
+    {
+        // throw invalid_argument("checker: expression begin or end with unclosed operation sign");
+        return false;
+    }
+    for (unsigned long long i = 0; i < expr.size(); i++)
+    {
+        char c = expr[i];
+        // проверка на отсутсвие лишних символов
+        if (!c_in_s(c, calc_chars))
+        {
+            // throw invalid_argument("checker: unknown character");
+            return false;
+        }
+        // возле знака операции не должно быть других операций
+        // (елси это не минус, так как он может быть унарный)
+        if (c_in_s(c, oper) && c != '-')
+        {
+            if ((c_in_s(expr[i - 1], oper) || c_in_s(expr[i + 1], oper)) &&
+                (expr[i - 1] != '-' && expr[i + 1] != '-'))
+            {
+                // throw invalid_argument("checker: multiple use of operation sign in one place");
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 // vector<char> transform_to_char (const vector<string>& strs)
 // {
@@ -69,16 +110,17 @@ double calc (const vector<string>& rev_pol, double x)
 {
     const string oper = "+-*/^";   // строка с операциями
     const string func = "sctelu";  // строка с функциями
-    // да, унарный минус - тоже функция
+                                   // (да, унарный минус - тоже функция)
 
     Stack stack;
     for (auto lex : rev_pol)
     {
         // stack.print();
-        double l, p;  // локальные переменные - последний и предпоследний символы в стеке
+        double l;  // последний символ в стэке
+        double p;  // предпоследний символ в стэке (последний после удаления l)
         char curr = transform_to_char(lex);  // текущий символ,
                                              // если брать вместо строки (для switch)
-        if (func.find(curr) != string::npos)  // получили функцию
+        if (c_in_s(curr, func))
         {
             l = stack.last();  // запоминаем только последний, так как операции унарны
             stack.pop();
@@ -104,7 +146,7 @@ double calc (const vector<string>& rev_pol, double x)
                 break;
             }
         }
-        else if (oper.find(curr) != string::npos)  // получили операцию
+        else if (c_in_s(curr, oper))
         {
             l = stack.last();
             stack.pop();
@@ -129,7 +171,7 @@ double calc (const vector<string>& rev_pol, double x)
                 break;
             }
         }
-        else  // получили число, x или ошибку
+        else
         {
             switch (curr)
             {
